fdff2b034be6b9329d3592f0dc32f7e1c862711a
Juan_C_Hernandez <33169379+JuanC-Hernandez@users.noreply.github.com>
2024-02-04 15:11:46 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lecture_Practice___Inheritance_JuanHernandez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_Practice___Inheritance_JuanHernandez
+{
+    internal class Vehicle
+    {
+        // Field
+        public string _color;
+        // Properties
+
+    }
+}
